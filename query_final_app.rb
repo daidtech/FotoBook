fb25098525d41c568f9836f642a0fc6ca3d46423
dinhvandai63 <dvdai63@gmail.com>
2020-09-16 09:03:47 +0700

    update query

--- query_final_app.rb
+++ query_final_app.rb
@@ -1,49 +1,62 @@
-# #################### Feeds #####################
-# # get public photo
-# user = User.find(11)
-# public_photos = user.photo.where(sharing_mode: "public").order(updated_at: :desc)
-# #get total heart
-# public_photos.each do |photo|
-#   puts photo.like.count
-# end
+#################### Feeds #####################
+# get public photo
+user = User.find(1)
+public_photos = user.photos.where(sharing_mode: "public").order(updated_at: :desc)
+#get total heart
+public_photos.each do |photo|
+  puts photo.likes.count
+end
 
-# #get public album
-# user = User.find(1)
-# public_albums = user.album.where(sharing_mode: "public").order(updated_at: :desc)
-# #get total heart
-# public_albums.each do |photo|
-#   puts photo.like.count
-# end
-# #################### Disovery #####################
-# photos = Photo.where(sharing_mode: "public").order(updated_at: :desc)
-# albums = Album.where(sharing_mode: "public").order(updated_at: :desc)
+#get public album
+user = User.find(1)
+public_albums = user.albums.where(sharing_mode: "public").order(updated_at: :desc)
+#get total heart
+public_albums.each do |photo|
+  puts photo.likes.count
+end
+#################### Disovery #####################
+photos = Photo.where(sharing_mode: "public").order(updated_at: :desc)
+albums = Album.where(sharing_mode: "public").order(updated_at: :desc)
 
-# ##############Public Profile
-# ###########Tab Photos
-# user_id = params[:user_id]
-# user_id = 11
-# User.find(user_id).photos.where(sharing_mode: "public")
-# ###########Tab Albums
-# User.find(user_id).albums.where(sharing_mode: "public")
-# ###########Tab Followings
-# User.find(user_id).followers
-# ###########Tab Followers
-# User.find(user_id).received_follows
+##############Public Profile
+###########Tab Photos
+# params[:user_id]
+user_id = params[:user_id]
+user_id = 1
+photos = User.find(user_id).photos.where(sharing_mode: "public")
+count_public_photos = photos.count
+###########Tab Albums
+albums = User.find(user_id).albums.where(sharing_mode: "public")
+count_public_albums = albums.count
+#get total photo on album
+albums.each do |album|
+  puts album.photos.count
+end
+###########Tab Followings
+following = User.find(user_id).followings
+count_following = following.count
+###########Tab Followers
+followers = User.find(user_id).followers
+count_followers = followers.count
 
-# ##############My Profile'
-# ###########Tab Photos
-# User.find(user_id).photos
-# ###########Tab Albums
-# User.find(user_id).albums
-# ###########Tab Followings
-# User.find(user_id).followers
-# ###########Tab Followers
-# User.find(user_id).received_follows
+############## My Profile #####################
+###########Tab Photos
+photos = User.find(user_id).photos
+count_photos = photos.count
+###########Tab Albums
+albums = User.find(user_id).albums
+count_albums = albums.count
+###########Tab Followings
+following = User.find(user_id).followings
+count_following = following.count
+###########Tab Followers
+followers = User.find(user_id).followers
+count_following = followers.count
 
-# ##############Admin dedicated features
-# ###########Manage Photos
-# Photo.all
-# ###########Manage Albums
-# Album.all
-# ###########Manage Users
-# User.all
+############## Admin dedicated features #####################
+###########Manage Photos
+Photo.all
+###########Manage Albums
+Album.all
+###########Manage Users
+User.all
