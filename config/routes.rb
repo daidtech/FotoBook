c2116cb4eb6eeec2eced176289e5d5b80fdb7529
dinhvandai63 <dvdai63@gmail.com>
2020-09-16 14:02:47 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,39 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/feeds', to: 'application#feeds'
-  get '/discover', to: 'application#discover'
+  # devise_for :users
 
-  resources :users, only: [:show] do
-    resources :photos, only: [:index]
+  get '/feeds/photos', to: 'application#feeds_photos'
+  get '/feeds/albums', to: 'application#feeds_albums'
+  get '/discover/photos', to: 'application#discover_photos'
+  get '/discover/albums', to: 'application#discover_albums'
+
+  namespace :profile do
+    get '/photos', to: 'public_profiles#photos'
+    get '/albums', to: 'public_profiles#albums'
+    get '/followers', to: 'public_profiles#follow'
+    get '/followings', to: 'public_profiles#follow'
+
+    get '/myphotos', to: 'my_profiles#photos'
+    get '/myalbums', to: 'my_profiles#albums'
+
+    get '/myfollowers', to: 'public_profiles#follow'
+    get '/myfollowings', to: 'public_profiles#follow'
+
+    get '/edit', to: 'my_profiles#edit'
+    put '/', to: 'my_profiles#update'
   end
+
   resources :photos
-  # scope module: 'admin' do
-  #   resources :articles, :comments
-  # end
+  resources :albums
+
+  resources :albums, only: [] do
+    resources :photos, only: [:index, :show]
+  end
+
+  scope :admin do
+    resources :photos, except: [:new, :create]
+    resources :albums, except: [:new, :create]
+    resources :users, except: [:new, :create]
+  end
+
+  root to: "application#feeds"
 end
