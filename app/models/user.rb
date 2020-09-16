7ad158d7564099f4f11d2ec93d694387466e2b8d
dinhvandai63 <dvdai63@gmail.com>
2020-09-16 09:03:02 +0700

    add relationship  User-Follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
   # Will return an array of users who follow the user instance
   has_many :followers, through: :received_follows, source: :follower
 
+  #####################
+
+  # returns an array of follows a user gave to someone else
+  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+  # returns an array of other users who the user has followed
+  has_many :followings, through: :given_follows, source: :followed_user
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
