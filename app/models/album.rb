c9b63825b6a9a2be6204d3318e8b75f0b8c3d42b
dinhvandai63 <dvdai63@gmail.com>
2020-09-17 17:35:08 +0700

    dependent album-photos

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ApplicationRecord
   belongs_to :user
-  has_many :photos
+  has_many :photos, dependent: :destroy
   has_many :likes, as: :heart, dependent: :destroy
   validates :title, presence: true, length: {maximum: 140}
   validates :description, presence: true, length: {maximum: 300}
