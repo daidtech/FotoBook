e442106993a672d30f03f0183eed19da35de720c
dinhvandai63 <dvdai63@gmail.com>
2020-09-17 23:36:45 +0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+############ User ############
+10.times do |i|
+  u1 = User.create(email: "daidinh#{i}@gmail.com", password: "123123", first_name: "dai#{i}", last_name: "dinh#{i}")
+end
+
+########### Photo ############
+u1 = User.first
+20.times do |i|
+  if i<=10
+    Photo.create(title: "photo#{i}", description: "description photo#{i}", sharing_mode: "public", user: u1)
+  else
+    Photo.create(title: "photo#{i}", description: "description photo#{i}", sharing_mode: "private", user: u1)
+  end
+end
+
+########### Album ############
+20.times do |i|
+  if i<=10
+    Album.create(title: "photo#{i}", description: "description photo#{i}", sharing_mode: "public", user: u1)
+  else
+    Album.create(title: "photo#{i}", description: "description photo#{i}", sharing_mode: "private", user: u1)
+  end
+end
+
+########### Like ############
+(1..10).each do |i|
+  Like.create(user: User.find(i), heart: Photo.find(i))
+end
+(1..10).each do |i|
+  Like.create(user: User.find(i), heart: Album.find(i))
+end
+
+########### Follow ############
+(1..8).each do |i|
+  Follow.create(follower: User.find(i), followed_user: User.find(i+1))
+end
+(1..7).each do |i|
+  Follow.create(follower: User.find(1), followed_user: User.find(i+2))
+end
