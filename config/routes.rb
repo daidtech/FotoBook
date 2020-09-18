c8ef1fdfb168def0ca02736c01f7653f2f10ec71
dinhvandai63 <dvdai63@gmail.com>
2020-09-18 13:45:11 +0700

    udpate a to to link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,38 @@
 Rails.application.routes.draw do
   # devise_for :users
-  get '/users/login', to: 'users#login'
-  get '/users/signup', to: 'users#signup'
+  scope "(:locale)", locale: /en|vi/ do
+    get '/users/login', to: 'users#login'
+    get '/users/signup', to: 'users#signup'
 
-  get '/feeds/photos', to: 'home#feeds_photos'
-  get '/feeds/albums', to: 'home#feeds_albums'
-  get '/discover/photos', to: 'home#discover_photos'
-  get '/discover/albums', to: 'home#discover_albums'
+    get '/feeds/photos', to: 'home#feeds_photos'
+    get '/feeds/albums', to: 'home#feeds_albums'
+    get '/discover/photos', to: 'home#discover_photos'
+    get '/discover/albums', to: 'home#discover_albums'
 
-  #public profiles
-  resources :users, only: [] do
-    member do
-      get '/photos', to: 'public_profile/photos#index'
-      get '/albums', to: 'public_profile/albums#index'
-      get '/followers', to: 'followers#index'
-      get '/followings', to: 'followings#index'
+    #public profiles
+    resources :users, only: [] do
+      member do
+        get '/photos', to: 'public_profile/photos#index'
+        get '/albums', to: 'public_profile/albums#index'
+        get '/followers', to: 'followers#index'
+        get '/followings', to: 'followings#index'
+      end
     end
+
+    get '/followers', to: 'followers#index'
+    get '/followings', to: 'followings#index'
+
+    get '/profiles/edit', to: 'profiles#edit'
+    put '/profiles', to: 'profiles#update'
+
+    resources :photos
+    resources :albums
+
+    scope :admin do
+      resources :photos, except: [:new, :create]
+      resources :albums, except: [:new, :create]
+      resources :users, except: [:new, :create]
+    end
+    root to: "home#feeds_photos"
   end
-
-  get '/followers', to: 'followers#index'
-  get '/followings', to: 'followings#index'
-
-  get '/profiles/edit', to: 'profiles#edit'
-  put '/profiles', to: 'profiles#update'
-
-  resources :photos
-  resources :albums
-
-  scope :admin do
-    resources :photos, except: [:new, :create]
-    resources :albums, except: [:new, :create]
-    resources :users, except: [:new, :create]
-  end
-
-  root to: "home#feeds_photos"
 end
